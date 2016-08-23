18ee5bba9cb0746d68ada3ed6b458e5aeb6e39fa
ARUNANAGESWARI <ablyaruna@gmail.com>
2016-08-23 18:50:56 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,13 @@
+#include<conio.h> 
+void main() 
+ { 
+     int n, i, a = 0; 
+     scanf("%d",&n); 
+ for(i=1; i <= n; ++i) 
+     { 
+         a += i;  
+   } 
+
+ 
+     printf("Sum = %d",a); 
+} 
